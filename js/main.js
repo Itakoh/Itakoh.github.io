793c47a21b0e63dcf9b2c7f2884d72e72a2d2af4
Nobutaka Takushima <nobutaka.takushima@itakoh.co.jp>
2019-04-27 23:19:51 +0900

    fix ios safari bug

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     currentWidth = $('.panel-cover').width();
     if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed');
-      $('.content-wrapper').addClass('animated slideInRight');
+      //$('.panel-cover').addClass('panel-cover--collapsed');
+      //$('.content-wrapper').addClass('animated slideInRight');
     } else {
       $('.panel-cover').css('max-width',currentWidth);
       $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function() {} );
